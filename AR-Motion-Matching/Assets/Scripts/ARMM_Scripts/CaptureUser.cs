8cf71d194c0cccf02da3cd9a5b030b6e02775eff
merc <kansamas@gmail.com>
2020-04-22 23:39:09 +0800

    Updates
    Added a song as player motion is detected and ore to mainflow

--- AR-Motion-Matching/Assets/Scripts/ARMM_Scripts/CaptureUser.cs
+++ AR-Motion-Matching/Assets/Scripts/ARMM_Scripts/CaptureUser.cs
@@ -11,20 +11,17 @@ public class CaptureUser : MonoBehaviour
     string user_file_location = "";
     float capture_time = 0;
     string user_folder_path = "";
+    string user_sub_folder = "Captured_movement";
+    string user_file_path = "Users_movement";
+    string file_end = ".dat";
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        StartCoroutine(Set_path());
     }
 
     //Captures the body movements and writes their locations
-    void record_movement()    {        if (!File.Exists(user_file_location))        {            FileStream tech_file = File.Create(user_file_location);            write_body_part(tech_file);            tech_file.Close();        }        else if (File.Exists(user_file_location))        {            FileStream tech_file = File.Open(user_file_location, FileMode.Append);            write_body_part(tech_file);            tech_file.Close();        }    }
+    public void record_movement()    {        if (File.Exists(user_file_location))        {            FileStream tech_file = File.Open(user_file_location, FileMode.Append);            write_body_part(tech_file);            tech_file.Close();        }    }
 
     void write_body_part(FileStream fs)    {        List<BodySegment> bp_list = new List<BodySegment>();        BinaryFormatter bf = new BinaryFormatter();
         foreach (KeyValuePair<JointIndices3D, Transform> BodyPart in HumanBodyTracking.bodyJoints)
@@ -34,11 +31,28 @@ public class CaptureUser : MonoBehaviour
         bf.Serialize(fs, bp_list);
         capture_time += Time.deltaTime;    }
 
-    void Set_path()
-    { 
-#if UNITY_EDITOR         user_folder_path = "Assets";#elif UNITY_IOS        user_folder_path = Application.persistentDataPath;
-#elif UNITY_ANDROID        user_folder_path = Application.persistentDataPath;
+    IEnumerator Set_path()
+    {
+#if UNITY_EDITOR         user_folder_path = Path.Combine("Assets", user_sub_folder);
+#elif UNITY_IOS        user_folder_path = Path.Combine(Application.persistentDataPath,user_sub_folder); 
+#elif UNITY_ANDROID        user_folder_path = Path.Combine(Application.persistentDataPath,user_sub_folder);    
 #endif
+        var folder_status = Directory.CreateDirectory(user_folder_path);
+        yield return new WaitForSeconds(0.2f);
+        if (!Directory.Exists(user_folder_path))
+        {
+            Debug.LogError("Failed to create folder at: " + user_folder_path);
+        }
+        string file_name = Path.Combine(user_file_path, file_end);
+
+        user_file_location = Path.Combine(user_folder_path, file_name);
+
+        File.Create(user_file_location);
+
+        if (!File.Exists(user_file_location))
+        {
+            Debug.LogError("Failed to create file at: " + user_file_location);
+        }
     }
 
 }
--- AR-Motion-Matching/Assets/Scripts/ARMM_Scripts/MainFlow.cs
+++ AR-Motion-Matching/Assets/Scripts/ARMM_Scripts/MainFlow.cs
@@ -10,6 +10,7 @@ public class MainFlow : MonoBehaviour
     HumanBodyData BodyData;
     ObjectNorm obj_norm;
     CaptureUser cap_user;
+    AudioSource start_sound;
     bool started_recording = false;
     int frames_count = 0;
     float frame_rate = 1;
@@ -20,23 +21,30 @@ public class MainFlow : MonoBehaviour
         BodyData = GameObject.Find("HumanData").GetComponent<HumanBodyData>();
         obj_norm = GameObject.Find("ObjectNorm").GetComponent<ObjectNorm>();
         cap_user = GameObject.Find("CaptureUser").GetComponent<CaptureUser>();
+        start_sound = GameObject.Find("Start_sound").GetComponent<AudioSource>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!started_recording && HumanBodyTracking.Body_flag)
+        {
+            start_sound.Play();
+            started_recording = true;
+        }
         frames_count++;
         if(frames_count%frame_rate == 0)
         {
             if (HumanBodyTracking.Body_flag)
             {
-
+                cap_user.record_movement();
             }
             frames_count = 0;
         }
        
     }
+
 
 
 }
