c0bbc3608712b5abd7e8af08fdb90b0a2b65060b
merc <kansamas@gmail.com>
2020-05-11 22:34:07 +0800

    Feedback button
    Added a feedback button that sends to user to forms

---
+++ AR-Motion-Matching/Assets/Scripts/Feedback_Handler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feedback_Handler : MonoBehaviour
+{
+   public void send_to_feedback()
+    {
+        Application.OpenURL("https://forms.gle/rPksQM4pY5JyR4XX6");
+    }
+}
