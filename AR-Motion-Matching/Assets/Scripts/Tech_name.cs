fe484a5ab920b78c943abc917513b8c20bffb6b7
merc <kansamas@gmail.com>
2020-05-15 02:35:37 +0800

    ARSession Debugging

--- AR-Motion-Matching/Assets/Scripts/Tech_name.cs
+++ AR-Motion-Matching/Assets/Scripts/Tech_name.cs
@@ -1,32 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
 
-public class Tech_name : MonoBehaviour
-{
-    public string selected_tech_path;
-    public float obj_norm_score;
-    static bool created = false;
-
-    private void Awake()
+    public class Tech_name : MonoBehaviour
     {
-        if (!created)
+        public string selected_tech_path;
+        public float obj_norm_score;
+        UnityEngine.XR.ARFoundation.ARSessionState ARState;
+        static bool created = false;
+
+        private void Awake()
         {
-            DontDestroyOnLoad(this.gameObject);
-            created = true;
+            if (!created)
+            {
+                DontDestroyOnLoad(this.gameObject);
+                created = true;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+
         }
-        else
+        // Start is called before the first frame update
+        void Start()
         {
-            Destroy(this.gameObject);
+            ARState = UnityEngine.XR.ARFoundation.ARSession.state;
+            selected_tech_path = "";
+            obj_norm_score = 125f;
+        }
+
+        private void Update()
+        {
+        
+            if(ARState != UnityEngine.XR.ARFoundation.ARSession.state)
+            {
+                Debug.Log("ARSessionState: " + UnityEngine.XR.ARFoundation.ARSession.state);
+                Debug.Log("ARsessionState changed, checking aviliablity");
+                UnityEngine.XR.ARFoundation.ARSession.CheckAvailability();
+                Debug.Log("New ARSessionState: " + UnityEngine.XR.ARFoundation.ARSession.state);
+            
+            }
+            ARState = UnityEngine.XR.ARFoundation.ARSession.state;
         }
 
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        selected_tech_path = "";
-        obj_norm_score = 125f;
-    }
-
-}
