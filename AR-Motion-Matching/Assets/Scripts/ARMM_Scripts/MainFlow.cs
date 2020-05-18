ae9aba762e3faea559da44e7221631b2c8ae04de
merc <kansamas@gmail.com>
2020-05-18 23:56:09 +0800

    ADAPTIVE
    Made ARMm adaptive and all run in one run
    Problem with loading from ARMm to feedback

--- AR-Motion-Matching/Assets/Scripts/ARMM_Scripts/MainFlow.cs
+++ AR-Motion-Matching/Assets/Scripts/ARMM_Scripts/MainFlow.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections;using System.Collections.Generic;using System.IO;using System.Runtime.Serialization.Formatters.Binary;using UnityEngine;using UnityEngine.SceneManagement;using UnityEngine.UI;using UnityEngine.XR.ARFoundation;public class MainFlow : MonoBehaviour{    public ARCameraManager cam;    CaptureUser cap_user;    Load_Movement user_move;    Load_Movement tech_move;    AudioSource start_sound;    CompareMovements comp_move;    Tech_name technique_name;    float Start_frame_index;    bool started_recording = false;    bool Recording_flag = false;    int frames_count = 0;    float frame_rate = 1;    int corr_index = -1;    float object_norm_score = -1;
+using System.Collections;using System.Collections.Generic;using System.IO;using System.Runtime.Serialization.Formatters.Binary;using UnityEngine;using UnityEngine.SceneManagement;using UnityEngine.UI;using UnityEngine.XR.ARFoundation;public class MainFlow : MonoBehaviour{    public ARCameraManager cam;    public GameObject DEBUG_UI;    CaptureUser cap_user;    Load_Movement user_move;    Load_Movement tech_move;    AudioSource start_sound;    CompareMovements comp_move;    Tech_name technique_name;    float Start_frame_index;    bool started_recording = false;    bool Recording_flag = false;    int frames_count = 0;    float frame_rate = 1;    int corr_index = -1;    float object_norm_score = -1;
 
     // Start is called before the first frame update
     void Start()    {        cap_user = GameObject.Find("CaptureUser").GetComponent<CaptureUser>();        user_move = GameObject.Find("Data_Loader").GetComponent<Load_Movement>();        tech_move = GameObject.Find("Tech_Loader").GetComponent<Load_Movement>();        start_sound = GameObject.Find("Start_sound").GetComponent<AudioSource>();        comp_move = GameObject.Find("Compare").GetComponent<CompareMovements>();        technique_name = GameObject.Find("Selected_tech").GetComponent<Tech_name>();        Start_frame_index = -1;        //Debug.LogError("ARCamera Status: " + UnityEngine.XR.ARFoundation.ARSession.state + "\nAR Reason: " + UnityEngine.XR.ARFoundation.ARSession.notTrackingReason);        //Debug.Log("Trying to install ARKIT.");                //try
@@ -12,7 +12,10 @@ using System.Collections;using System.Collections.Generic;using System.IO;usi
         //}            }
 
     // Update is called once per frame
-    void Update()    {        if (!Recording_flag)        {            if (!started_recording && HumanBodyTracking.Body_flag)            {                start_sound.Play();                started_recording = true;            }            frames_count++;            if (frames_count % frame_rate == 0)            {                if (HumanBodyTracking.Body_flag)                {                    cap_user.record_movement();                }                frames_count = 0;            }        }
+    void Update()    {        if (technique_name.DEBUG_MODE)
+        {
+            DEBUG_UI.SetActive(true);
+        }        else if (!technique_name.DEBUG_MODE)        {            DEBUG_UI.SetActive(false);        }        if (!Recording_flag)        {            if (!started_recording && HumanBodyTracking.Body_flag)            {                start_sound.Play();                started_recording = true;            }            frames_count++;            if (frames_count % frame_rate == 0)            {                if (HumanBodyTracking.Body_flag)                {                    cap_user.record_movement();                }                frames_count = 0;            }        }
     }    public void stop_recording()    {        Recording_flag = true;
         //StartCoroutine(Load_movements());
         HumanBodyTracking.Body_flag = false;        Load_movements();    }
@@ -24,7 +27,13 @@ using System.Collections;using System.Collections.Generic;using System.IO;usi
         }        else        {            technique_name.obj_norm_score = object_norm_score;        }
 
         Load_Feedback();
-    }    public void Load_Feedback()    {        SceneManager.LoadScene("Feedback");    }    public void Load_MainUI()    {        SceneManager.LoadScene("MainUI");    }    public void Load_ARMm()    {        SceneManager.LoadScene("ARMm");    }
+    }    void Reset_ARMm()
+    {
+        started_recording = false;
+        Recording_flag = false;
+        technique_name.selected_tech_path = "";
+        technique_name.obj_norm_score = 127f;
+    }    public void Load_Feedback()    {        SceneManager.LoadSceneAsync("Feedback", LoadSceneMode.Additive);    }    public void Load_MainUI()    {        SceneManager.LoadSceneAsync("MainUI", LoadSceneMode.Additive);    }    public void Load_ARMm()    {        Reset_ARMm();        //SceneManager.LoadScene("ARMm");    }
 
 
 }
--- AR-Motion-Matching/Assets/Scripts/Tech_name.cs
+++ AR-Motion-Matching/Assets/Scripts/Tech_name.cs
@@ -7,6 +7,7 @@
     {
         public string selected_tech_path;
         public float obj_norm_score;
+    public bool DEBUG_MODE;
         UnityEngine.XR.ARFoundation.ARSessionState ARState;
         static bool created = false;
 
@@ -26,23 +27,24 @@
         // Start is called before the first frame update
         void Start()
         {
-            ARState = UnityEngine.XR.ARFoundation.ARSession.state;
+            //ARState = UnityEngine.XR.ARFoundation.ARSession.state;
             selected_tech_path = "";
             obj_norm_score = 125f;
+            DEBUG_MODE = false;
         }
 
-        private void Update()
-        {
+        //private void Update()
+        //{
         
-            if(ARState != UnityEngine.XR.ARFoundation.ARSession.state)
-            {
-                Debug.Log("ARSessionState: " + UnityEngine.XR.ARFoundation.ARSession.state);
-                Debug.Log("ARsessionState changed, checking aviliablity");
-                UnityEngine.XR.ARFoundation.ARSession.CheckAvailability();
-                Debug.Log("New ARSessionState: " + UnityEngine.XR.ARFoundation.ARSession.state);
+        //    if(ARState != UnityEngine.XR.ARFoundation.ARSession.state)
+        //    {
+        //        Debug.Log("ARSessionState: " + UnityEngine.XR.ARFoundation.ARSession.state);
+        //        Debug.Log("ARsessionState changed, checking aviliablity");
+        //        UnityEngine.XR.ARFoundation.ARSession.CheckAvailability();
+        //        Debug.Log("New ARSessionState: " + UnityEngine.XR.ARFoundation.ARSession.state);
             
-            }
-            ARState = UnityEngine.XR.ARFoundation.ARSession.state;
-        }
+        //    }
+        //    ARState = UnityEngine.XR.ARFoundation.ARSession.state;
+        //}
 
     }
